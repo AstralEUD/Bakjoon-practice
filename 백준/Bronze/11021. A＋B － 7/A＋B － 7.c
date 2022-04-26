49932339e0bcd82e2a99e145ab6ab34a5a6ba98e
AstralEUD <50492313+AstralEUD@users.noreply.github.com>
2022-04-26 12:08:51 +0900

    [Bronze III] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -2,8 +2,8 @@
 main () {
     int a, b, c;
     scanf("%d",&c);
-    for (int i=0; i<c; i++) {
-        scanf ("%d %d",&a,&b);
+    for (int i=0;i<c;i++) {
+        scanf("%d %d",&a,&b);
         printf("Case #%d: %d\n",i+1,a+b);
     }
 }
