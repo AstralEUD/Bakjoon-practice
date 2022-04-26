b57202c0b25314674fa96733c1dea3a6cf418749
AstralEUD <50492313+AstralEUD@users.noreply.github.com>
2022-04-26 12:00:47 +0900

    [Bronze III] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -1,8 +1,9 @@
+#include <stdio.h>
 main () {
     int a, b, c, i;
-    scanf("%d",&a);
-    for (i=0; i<a; ++i) {
-        scanf("%d %d",&b,&c);
-        printf("%d\n",b+c);
+    scanf("%d",&c);
+    for (i=0;i<c;i++) {
+        scanf("%d %d",&a,&b);
+        printf("%d\n",a+b);
     }
 }
