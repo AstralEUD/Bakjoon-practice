1a5e88b5a3874108658eba84931e0a676d959675
AstralEUD <50492313+AstralEUD@users.noreply.github.com>
2022-05-31 10:29:54 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,12 @@
+int main () {
+    int a;
+    scanf("%d",&a);
+    switch (a) {
+        case 0:
+            printf("YONSEI");
+            break;
+        default:
+            printf("Leading the Way to the Future");
+            break;
+    }
+}
