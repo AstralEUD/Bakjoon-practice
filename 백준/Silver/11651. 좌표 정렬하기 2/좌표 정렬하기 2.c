7e3d070935b120388b8810673d91592896b7fa7c
Testify <50492313+AstralEUD@users.noreply.github.com>
2024-04-27 23:17:04 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 80 ms, Memory: 2676 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  int x;
+  int y;
+} cordinate;
+
+cordinate arr[100005];
+
+int compare_x(const void *a, const void *b) {
+  cordinate *structA = (cordinate *)a;
+  cordinate *structB = (cordinate *)b;
+
+  return (structA->x - structB->x);
+}
+
+int compare_y(const void *a, const void *b) {
+  cordinate *structA = (cordinate *)a;
+  cordinate *structB = (cordinate *)b;
+
+  return (structA->y - structB->y);
+}
+
+int main(void) {
+  int n, i, p;
+  scanf("%d", &n);
+  for (i = 0; i < n; i++) {
+    scanf("%d %d", &arr[i].x, &arr[i].y);
+  }
+  qsort(arr, n, sizeof(cordinate), compare_x);
+  qsort(arr, n, sizeof(cordinate), compare_y);
+  for (i = 0; i < n; i++) {
+    printf("%d %d \n", arr[i].x, arr[i].y);
+  }
+  return 0;
+}
