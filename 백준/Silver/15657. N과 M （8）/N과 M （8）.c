dbd12c79bf31faff6c2789c781bff949e2a985f3
Testify <50492313+AstralEUD@users.noreply.github.com>
2024-08-22 23:33:01 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int arr[10] = {0};
+int pred[10] = {0};
+int length = 0;
+
+int checkIfNumInArr(int n) {
+  for (int i = 0; i < length; i++) {
+    if (arr[i] == n)
+      return TRUE;
+  }
+  return FALSE;
+}
+
+int compare(const void *a, const void *b) { return (*(int *)a - *(int *)b); }
+
+void Backtracking(int n, int m) {
+  if (length == m) {
+    for (int i = 0; i < m; i++) {
+      printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return;
+  }
+  for (int i = 1; i <= n; i++) {
+    if (((length == 0) || (pred[i - 1] >= arr[length - 1]))) {
+      arr[length] = pred[i - 1];
+      length++;
+      Backtracking(n, m);
+      arr[length] = 0;
+      length--;
+    }
+  }
+}
+
+int main(void) {
+  int n, m, i, j, p, q, r, s, aa, ab, ac;
+
+  scanf("%d %d", &n, &m);
+  for (i = 0; i < n; i++) {
+    scanf("%d", &pred[i]);
+  }
+
+  qsort(pred, n, sizeof(int), compare);
+
+  Backtracking(n, m);
+
+  return 0;
+}
